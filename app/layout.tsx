69206bb1f61eda5c228ad80b350b6b8571c195b0
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-02 22:21:38 +0530

    completed setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "EduSync",
+    description:
+        "EduSync is a online LMS platform. Teachers can upload their courses and students can learn from them.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <p className="text-2xl font-medium text-sky-500">Hello World</p>
+            <Button> Click me</Button>
+        </div>
+    );
+}
