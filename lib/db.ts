5382c1297d805af5415c50dbe1971785d01cfae7
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-06 00:54:25 +0530

    Refactor db.ts to use globalThis.prisma or create a new PrismaClient instance

--- lib/db.ts
+++ lib/db.ts
@@ -4,15 +4,7 @@ declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const db =
-    globalThis.prisma ||
-    new PrismaClient({
-        datasources: {
-            db: {
-                url: process.env.DATABASE_URL,
-            },
-        },
-    });
+export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
     globalThis.prisma = db;
