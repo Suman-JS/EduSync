1a4e7229b6846929ec06b2784851816e6a7a9cec
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-07 18:33:35 +0530

    Fix Dockerfile and Popover component  and combobox component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import React from "react";
+import { LayoutDashboard, ListChecks } from "lucide-react";
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/IconBadge";
-import { LayoutDashboard } from "lucide-react";
-import TitleForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/TitleForm";
-import DescriptionForm from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/DescriptionForm";
-import { ImageForm } from "@/app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ImageForm";
+
+import TitleForm from "./_components/TitleForm";
+import DescriptionForm from "./_components/DescriptionForm";
+import { ImageForm } from "./_components/ImageForm";
+import { CategoryForm } from "./_components/CategoryForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -15,9 +16,17 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     if (!userId) {
         return redirect("/");
     }
+
     const course = await db.course.findUnique({
         where: {
             id: params.courseId,
+            userId,
+        },
+    });
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc",
         },
     });
 
@@ -39,33 +48,40 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const completionText = `(${completedFields}/${totalFields})`;
 
     return (
-        <div className="p-6">
-            <div className="flex items-center justify-between">
-                <div className="flex flex-col gap-y-2">
-                    <h1 className="text-2xl font-medium">Course setup</h1>
-                    <span className="text-sm text-slate-700">
-                        Complete all fields {completionText}
-                    </span>
-                </div>
-            </div>
-            <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={LayoutDashboard} />
-                        <h2 className="text-xl">Customize your course</h2>
+        <>
+            <div className="p-6">
+                <div className="flex items-center justify-between">
+                    <div className="flex flex-col gap-y-2">
+                        <h1 className="text-2xl font-medium">Course setup</h1>
+                        <span className="text-sm text-slate-700">
+                            Complete all fields {completionText}
+                        </span>
                     </div>
+                </div>
+                <div className="mt-16 grid grid-cols-1 gap-6 md:grid-cols-2">
                     <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={LayoutDashboard} />
+                            <h2 className="text-xl">Customize your course</h2>
+                        </div>
                         <TitleForm initialData={course} courseId={course.id} />
                         <DescriptionForm
                             initialData={course}
                             courseId={course.id}
                         />
-
                         <ImageForm initialData={course} courseId={course.id} />
+                        <CategoryForm
+                            initialData={course}
+                            courseId={course.id}
+                            options={categories.map((category) => ({
+                                label: category.name,
+                                value: category.id,
+                            }))}
+                        />
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -11,6 +11,7 @@ import {
     CommandGroup,
     CommandInput,
     CommandItem,
+    CommandList,
 } from "@/components/ui/command";
 import {
     Popover,
@@ -18,14 +19,11 @@ import {
     PopoverTrigger,
 } from "@/components/ui/popover";
 
-type ComboboxProps = {
-    options: {
-        label: string;
-        value: string;
-    }[];
+interface ComboboxProps {
+    options: { label: string; value: string }[];
     value?: string;
     onChange: (value: string) => void;
-};
+}
 
 export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
     const [open, setOpen] = React.useState(false);
@@ -37,7 +35,7 @@ export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
                     variant="outline"
                     role="combobox"
                     aria-expanded={open}
-                    className="w-[200px] justify-between"
+                    className="w-full justify-between"
                 >
                     {value
                         ? options.find((option) => option.value === value)
@@ -46,15 +44,15 @@ export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
                     <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                 </Button>
             </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0">
+            {/* <PopoverContent className="w-full p-0">
                 <Command>
                     <CommandInput placeholder="Search option..." />
+
                     <CommandEmpty>No option found.</CommandEmpty>
                     <CommandGroup>
                         {options.map((option) => (
                             <CommandItem
                                 key={option.value}
-                                value={option.value}
                                 onSelect={() => {
                                     onChange(
                                         option.value === value
@@ -64,17 +62,53 @@ export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
                                     setOpen(false);
                                 }}
                             >
-                                <Check
-                                    className={cn(
-                                        "mr-2 h-4 w-4",
-                                        value === option.value
-                                            ? "opacity-100"
-                                            : "opacity-0"
-                                    )}
-                                />
+                                <CommandList>
+                                    <Check
+                                        className={cn(
+                                            "mr-2 h-4 w-4",
+                                            value === option.value
+                                                ? "opacity-100"
+                                                : "opacity-0"
+                                        )}
+                                    />
+                                </CommandList>
                                 {option.label}
                             </CommandItem>
                         ))}
+                    </CommandGroup>
+                </Command>
+            </PopoverContent> */}
+
+            <PopoverContent className="w-full p-0">
+                <Command>
+                    <CommandInput placeholder="Search option..." />
+                    <CommandEmpty>No option found.</CommandEmpty>
+                    <CommandGroup>
+                        <CommandList>
+                            {options.map((option) => (
+                                <CommandItem
+                                    key={option.value}
+                                    onSelect={() => {
+                                        onChange(
+                                            option.value === value
+                                                ? ""
+                                                : option.value
+                                        );
+                                        setOpen(false);
+                                    }}
+                                >
+                                    <Check
+                                        className={cn(
+                                            "mr-2 h-4 w-4",
+                                            value === option.value
+                                                ? "opacity-100"
+                                                : "opacity-0"
+                                        )}
+                                    />
+                                    {option.label}
+                                </CommandItem>
+                            ))}
+                        </CommandList>
                     </CommandGroup>
                 </Command>
             </PopoverContent>
