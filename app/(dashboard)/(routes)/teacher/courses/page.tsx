031e56c6245fbdb6e56a5e3ef6d36c9d445ca8b7
Suman Mondal <suman.mondal@techriskpartners.com>
2024-04-18 21:38:21 +0530

    feat: added datatable for displaying courses in teacher's courses page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "@/app/(dashboard)/(routes)/teacher/courses/_components/DataTable";
+import { columns } from "@/app/(dashboard)/(routes)/teacher/courses/_components/Columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursePage = () => {
+const CoursePage = async () => {
+    const { userId } = auth();
+    if (!userId) {
+        return redirect("/");
+    }
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
     return (
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>New Course</Button>
-            </Link>
+            <DataTable columns={columns} data={courses} />
         </div>
     );
 };
